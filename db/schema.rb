3c8b4612a20134b220207bb64d8d44b3d0bfcac7
kouhei.umeki <kouhei11.u@gmail.com>
2023-11-05 04:06:45 +0000

    status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_29_142617) do
+ActiveRecord::Schema.define(version: 2023_11_05_030310) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
