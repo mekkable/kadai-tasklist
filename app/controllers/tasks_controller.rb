118c65d3a95fd0cb910ed30fb0701d7716279bf3
kouhei.umeki <kouhei11.u@gmail.com>
2023-11-08 00:38:56 +0000

    login_identification

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,17 @@
 class TasksController < ApplicationController
+  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in
+  
   def index
-    @tasks=Task.all
+    
+    unless logged_in?
+      redirect_to login_path
+    else
+      @task = current_user.tasks.build
+      @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
+    end
+    
+    
   end
 
   def show
@@ -12,8 +23,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
+   
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
@@ -40,7 +51,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task= Task.find(params[:id])
+    
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,8 +60,14 @@ class TasksController < ApplicationController
   
   private
 
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to:'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:create]
   resources :tasks
 end
