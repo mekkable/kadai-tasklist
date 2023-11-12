6d5e5c096fea03bd8433afbba4bac08d3e508202
kouhei.umeki <kouhei11.u@gmail.com>
2023-11-12 14:08:19 +0000

    heeoku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_07_150238) do
+ActiveRecord::Schema.define(version: 2023_11_10_215915) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
