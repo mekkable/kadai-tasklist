463616c1403c3163de6ea13e6cb967796b21ddbb
kouhei.umeki <kouhei11.u@gmail.com>
2023-11-16 02:40:26 +0000

    fforkeroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :correct_user, only: [:destroy, :show]
+  before_action :correct_user
   before_action :require_user_logged_in
   
   def index
